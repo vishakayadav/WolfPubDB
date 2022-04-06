05ca41b903c38c9b9a62be6566774368eb892b28
Vishaka2502 <vishaka2502@gmail.com>
2022-04-05 21:41:47 -0400

    [add]: steps to execute in readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # WolfPubDB
+
+### Steps to execute
+- `cd WolfPubDB`
+- `cp env_template .env`
+- set values for Maria DB connection
+- `pip install -r requirements.txt`
+- `python setup.py install`
+- `python wolfpub/run.py`
--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup, find_packages
+from wolfpub.config import API_SETTINGS
+
 setup(
     name='WolfPub',
-    version='1.0.0',
+    version=API_SETTINGS['VERSION'],
     packages=find_packages(),
     include_package_data=True,
     data_files=[('wolfpub', ['wolfpub/settings.conf'])]
